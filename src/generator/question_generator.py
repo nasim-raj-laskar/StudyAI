9b525209f8d9b348dd5323b9980c1d23491f5840
nasim-raj-laskar <nosytub@gmail.com>
2026-01-28 00:28:21 +0530

    question_generator

---
+++ src/generator/question_generator.py
@@ -0,0 +1,60 @@
+from src.models.question_schemas import MCQQuestion, FillBlankQuestion
+from src.llm.groq_client import get_groq_llm
+from src.prompts.templates import mcq_prompt_template, fill_blank_prompt_template
+from src.config.setting import settings
+from src.common.logger import get_logger
+from src.common.custom_exception import CustomException
+
+
+class QuestionGenerator:
+    def __init__(self):
+        self.llm = get_groq_llm()
+        self.logger = get_logger(self.__class__.__name__)
+
+        # Structured LLMs
+        self.mcq_llm = self.llm.with_structured_output(MCQQuestion)
+        self.fill_blank_llm = self.llm.with_structured_output(FillBlankQuestion)
+
+    def _retry_and_generate(self, structured_llm, prompt_template, topic, difficulty):
+        for attempt in range(settings.MAX_RETRIES):
+            try:
+                self.logger.info(f"Generating question for topic: {topic}, difficulty: {difficulty}, attempt: {attempt + 1}")
+
+                prompt = prompt_template.format(topic=topic, difficulty=difficulty)
+                question = structured_llm.invoke(prompt)
+
+                self.logger.info(f"Successfully generated question on attempt {attempt + 1}")
+                return question
+
+            except Exception as e:
+                self.logger.error(f"Attempt {attempt + 1} failed: {str(e)}")
+                if attempt == settings.MAX_RETRIES - 1:
+                    raise CustomException(f"Failed to generate question after {settings.MAX_RETRIES} attempts", e)
+
+    def generate_mcq(self, topic: str, difficulty: str = "medium") -> MCQQuestion:
+        try:
+            question = self._retry_and_generate(self.mcq_llm,mcq_prompt_template,topic,difficulty)
+
+            if len(question.options) != 4 or question.correct_option not in question.options:
+                raise CustomException("Invalid MCQ format received from LLM")
+
+            self.logger.info("Generated MCQ question successfully")
+            return question
+
+        except Exception as e:
+            self.logger.error(f"Error generating MCQ question: {str(e)}")
+            raise CustomException("Error generating MCQ question", e)
+
+    def generate_fill_blank(self, topic: str, difficulty: str = "medium") -> FillBlankQuestion:
+        try:
+            question = self._retry_and_generate(self.fill_blank_llm,fill_blank_prompt_template,topic,difficulty)
+
+            if "_____" not in question.question_text or not question.correct_answer:
+                raise CustomException("Invalid Fill-in-the-Blank format received from LLM")
+
+            self.logger.info("Generated Fill-in-the-Blank question successfully")
+            return question
+
+        except Exception as e:
+            self.logger.error(f"Error generating Fill-in-the-Blank question: {str(e)}")
+            raise CustomException("Error generating Fill-in-the-Blank question", e)
